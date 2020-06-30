b5aed80618b18853d72b2a4f8cb2d1f71ff1c8d6
kolos <koloskovain20@yandex.ru>
2020-06-30 14:48:55 +0300

    Подготовка web-контента Spring MVC

--- src/main/java/root/Application.java
+++ src/main/java/root/Application.java
@@ -11,15 +11,16 @@ import root.entity.Category;
 @EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class);
-        CategoryRepository  repository= context.getBean(CategoryRepository.class);
-        repository.save(new Category("New message 1"));
-        repository.save(new Category("New message 2"));
-        Iterable<Category> lists = repository.findAll();
+        //ConfigurableApplicationContext context = SpringApplication.run(Application.class);
+       // CategoryRepository  repository= context.getBean(CategoryRepository.class);
+        //repository.save(new Category("New message 1"));
+        //repository.save(new Category("New message 2"));
+       // Iterable<Category> lists = repository.findAll();
 
-        for (Category entity: lists){
-            System.out.println(entity.getName());
-        }
-        context.close();
+       // for (Category entity: lists){
+       //     System.out.println(entity.getName());
+       // }
+       // context.close();
+        SpringApplication.run(Application.class, args);
     }
 }
