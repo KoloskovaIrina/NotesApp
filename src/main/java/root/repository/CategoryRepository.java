469a6515288b99d0c31106a668d85ddf96ad89bf
KoloskovaIrina <48804267+KoloskovaIrina@users.noreply.github.com>
2020-07-02 13:56:49 +0300

    Update CategoryRepository.java

--- src/main/java/root/repository/CategoryRepository.java
+++ src/main/java/root/repository/CategoryRepository.java
@@ -1,8 +1,2 @@
-package root.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import root.entity.Category;
-
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-}
 
